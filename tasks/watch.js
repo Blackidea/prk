b9baf98ee2a69ad613bb592ad7dc828b84d28000
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-13 11:20:46 +0300

    fix task styles

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,9 +3,14 @@ import watch from 'gulp-watch';
 import { bs } from './server';
 
 export default () => {
-    watch(['./app/styles/club/**/*.scss'], gulp.series('styles'));
-    watch(['./app/**/*.pug'], gulp.series('templates', bs.reload));
-    watch(['./app/resources/**/*.*'], gulp.series('resources'));
-    watch(['./app/icons/club/*.svg'], gulp.series('icons'));
-    watch(['./dist/*.html'], bs.reload);
+  const stylesPath = [
+    './app/styles/club/blocks/*.scss',
+    './app/styles/club/global/*.scss',
+    './app/styles/club/*.scss'
+  ];
+  watch(stylesPath, gulp.series('styles'));
+  watch(['./app/**/*.pug'], gulp.series('templates', bs.reload));
+  watch(['./app/resources/**/*.*'], gulp.series('resources'));
+  watch(['./app/icons/club/*.svg'], gulp.series('icons'));
+  watch(['./dist/*.html'], bs.reload);
 };
