39839af564c3856c83c6d0870030b6e09a8b15f0
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-16 00:56:19 +0300

    fix icons sprite

--- tasks/icons.js
+++ tasks/icons.js
@@ -1,32 +1,29 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import notify from 'gulp-notify';
+import svgSymbols from 'gulp-svg-symbols';
 import gulpIf from 'gulp-if';
-import svgSprite from 'gulp-svg-sprite';
+import rename from 'gulp-rename';
+import path from 'path';
 
 export default () => (
-    gulp.src('./app/icons/club/*.svg')
-        .pipe(plumber({
-            errorHandler: notify.onError(err => ({
-                title: 'icons',
-                message: err.message
-            }))
-        }))
-        .pipe(svgSprite({
-            mode: {
-                css: {
-                    dest: '.',
-                    bust: false,
-                    sprite: '../images/xfo-svg__icons.svg',
-                    prefix: '%xfo-',
-                    layout: 'vertical',
-                    dimensions: true,
-                    render: {
-                        scss: {
-                            dest: 'icons.scss'
-                        }
-                    }
-                }
-            }
-        }))
-        .pipe(gulpIf('*.svg', gulp.dest('dist/assets/images'), gulp.dest('./app/styles/club/global'))))
+  gulp.src('./app/icons/club/*.svg')
+    .pipe(plumber({
+        errorHandler: notify.onError(err => ({
+          title: 'icons',
+        message: err.message
+      }))
+    }))
+    .pipe(svgSymbols({
+      id: 'xfo-svg__%f',
+      className: '.xfo-svg__%f',
+      templates: [
+        path.join(__dirname, 'icons.scss'),
+        'default-svg'
+      ]
+    }))
+    .pipe(gulpIf('*.svg', rename('xfo-svg__icons.svg')))
+    .pipe(gulpIf('*.scss', rename('icons.scss')))
+    .pipe(gulpIf('*.svg', gulp.dest('dist/assets/images')))
+    .pipe(gulpIf('*.scss', gulp.dest('app/styles/global/')))
+);
