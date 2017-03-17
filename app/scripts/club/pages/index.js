69b2b0192d44ca71d842d4bee0addb9a4acb8c67
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-17 22:23:26 +0300

    add jquery

--- app/scripts/club/pages/index.js
+++ app/scripts/club/pages/index.js
@@ -3,6 +3,8 @@ if (!(window.XFO instanceof Object)) {
   window.XFO = {};
 }
 
+//= ../../../../bower_components/jquery/dist/jquery.js
+
 //= ../../../../bower_components/swiper/dist/js/swiper.js
 //= ../blocks/banner-slider.js
 //= ../blocks/partners-slider.js
