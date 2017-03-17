56dc0065e4005f630478bfa26a2c3b03b1bdfd29
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-17 18:24:52 +0300

    fix certificates slider

--- app/scripts/club/pages/certificates.js
+++ app/scripts/club/pages/certificates.js
@@ -3,7 +3,13 @@ if (!(window.XFO instanceof Object)) {
   window.XFO = {};
 }
 
+//= ../../../../bower_components/swiper/dist/js/swiper.js
+const bannerSlider = new Swiper('.jso-banner-slider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // autoplay: true
+});
+
 //= ../blocks/popup.js
 //= ../blocks/buy-card.js
-//= ../blocks/carousel.js
-//= ../blocks/banner-slider.js
