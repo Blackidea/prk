84814b04e42d5824c939ac1939009c416f03fe55
Ilya <rewasd91@mail.ru>
2017-03-21 01:21:51 +0300

    coupon page

---
+++ app/scripts/club/pages/coupon.js
@@ -0,0 +1,9 @@
+// Инициализация глобального объекта XFO
+if (!(window.XFO instanceof Object)) {
+  window.XFO = {};
+}
+
+//= ../../../../bower_components/jquery/dist/jquery.js
+//= ../../../../bower_components/swiper/dist/js/swiper.js
+
+//= ../blocks/banner-slider.js
