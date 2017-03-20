5b35d938dca734e3fe083f41fa32523849e9fde3
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-03-20 13:28:31 +0300

    bank-cards mobile

---
+++ app/scripts/club/pages/bank-cards.js
@@ -0,0 +1,10 @@
+// Инициализация глобального объекта XFO
+if (!(window.XFO instanceof Object)) {
+  window.XFO = {};
+}
+
+//= ../../../../bower_components/jquery/dist/jquery.js
+//= ../../../../bower_components/swiper/dist/js/swiper.js
+
+//= ../blocks/popup.js
+//= ../blocks/banner-slider.js
